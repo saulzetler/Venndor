c755edbfdf11e0ccea8089f347d25977c0ed1c0e
Tynan Davis <Tynandavis@gmail.com>
2016-06-08 15:22:57 -0400

    working facebook login

---
+++ venndor/venndor/LoginViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  venndor
+//
+//  Created by Tynan Davis on 2016-06-07.
+//  Copyright © 2016 Venndor. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+    let loginButton : FBSDKLoginButton = FBSDKLoginButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.addSubview(loginButton)
+        loginButton.center = self.view.center
+        loginButton.readPermissions = ["public_profile", "email"]
+        loginButton.delegate = self
+        
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+            //fetchProfile()
+            self.performSegueWithIdentifier("showNew", sender: self)
+            
+        }
+        else
+        {
+            print("Already logged in")
+        }
+        
+        
+    }
+/*    func fetchProfile() {
+        print("fetch profile")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler {(
+            connection)}
+    }*/
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error == nil
+        {
+            print("Login complete")
+            self.performSegueWithIdentifier("showNew", sender: self)
+        }
+        else
+        {
+            print(error.localizedDescription)
+        }
+    }
+    
+    //FACEBOOK LOGIN DELEGATES
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("logged out")
+    }
+    
+}
+
