b970f22059d53e8c8c8eaeffc5a8feaa2587a4f7
Tynan Davis <StrawberryCheesecake@users.noreply.github.com>
2016-06-08 10:23:17 -0400

    Add files via upload
    Initial Project, with some Facebook integration.

---
+++ venndor/venndor-Bridging-Header.h
@@ -0,0 +1,42 @@
+//
+//  venndor-Bridging-Header.h
+//  venndor
+//
+//  Created by Tynan Davis on 2016-06-07.
+//  Copyright © 2016 Venndor. All rights reserved.
+//
+
+#ifndef venndor_Bridging_Header_h
+#define venndor_Bridging_Header_h
+
+
+#endif /* venndor_Bridging_Header_h */
+
+#import "FBSDKAccessToken.h"
+#import "FBSDKAppEvents.h"
+#import "FBSDKApplicationDelegate.h"
+#import "FBSDKAppLinkResolver.h"
+#import "FBSDKAppLinkUtility.h"
+#import "FBSDKButton.h"
+#import "FBSDKConstants.h"
+#import "FBSDKCopying.h"
+#import "FBSDKCorekit.h"
+#import "FBSDKGraphErrorRecoveryProcessor.h"
+#import "FBSDKGraphRequest.h"
+#import "FBSDKGraphRequestDataAttachment.h"
+#import "FBSDKGraphRequestConnection.h"
+#import "FBSDKMacros.h"
+#import "FBSDKMutableCopying.h"
+#import "FBSDKProfile.h"
+#import "FBSDKProfilePictureView.h"
+#import "FBSDKSettings.h"
+#import "FBSDKFBSDKTestUsersManager.h"
+#import "FBSDKUtility.h"
+
+#import "FBSDKLoginButton.h"
+#import "FBSDKLoginConstants.h"
+#import "FBSDKLoginKit.h"
+#import "FBSDKLoginManager.h"
+#import "FBSDKLoginTooltopView.h"
+#import "FBSDKTooltopView.h"
+#import "FBSDKLoginManagerLoginResult.h"
---
+++ venndor/venndor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  venndor
+//
+//  Created by Tynan Davis on 2016-06-07.
+//  Copyright © 2016 Venndor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
