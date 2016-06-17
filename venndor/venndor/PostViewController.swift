181d25ed37ab9b941d988e870dc22c94444ba623
saulzetler <saul.zetler49@gmail.com>
2016-06-17 13:06:23 -0400

    some fixes

--- venndor/venndor/PostViewController.swift
+++ venndor/venndor/PostViewController.swift
@@ -15,10 +15,10 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     let screenSize: CGRect = UIScreen.mainScreen().bounds
     var itemDescription: UITextView!
     var itemName: UITextField!
-    var imageView: UIImageView!
+    var imageView1: UIImageView!
+    var imageView2: UIImageView!
+    var imageView3: UIImageView!
     var postButton: UIButton!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,7 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         setupItemDescription()
         sideMenuGestureSetup()
         addHeader()
+        setupImageViews()
     }
     
     func setupItemName() {
@@ -33,11 +34,7 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         itemName.text = "Name"
         itemName.clearsOnBeginEditing = true
         self.view.addSubview(itemName)
-        let boarderColor : UIColor = UIColor.blackColor()
-        itemName.layer.borderColor = boarderColor.CGColor
-        itemName.layer.borderWidth = 2.0
-        itemName.layer.cornerRadius = 8.0
-        itemName.layer.masksToBounds = true
+        createBoarder(itemName)
     }
     
     func setupItemDescription() {
@@ -45,18 +42,68 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         itemDescription.text = "Description"
         itemDescription.delegate = self
         self.view.addSubview(itemDescription)
+        createBoarder(itemDescription)
+        
+    }
+    
+    
+    
+    func setupImageViews() {
+        imageView1 = UIImageView(frame: CGRectMake(10, 230, self.screenSize.width*0.3, self.screenSize.width*0.3))
+        imageView2 = UIImageView(frame: CGRectMake(145, 230, self.screenSize.width*0.3, self.screenSize.width*0.3))
+        imageView3 = UIImageView(frame: CGRectMake(280, 230, self.screenSize.width*0.3, self.screenSize.width*0.3))
+        self.view.addSubview(imageView1)
+        self.view.addSubview(imageView2)
+        self.view.addSubview(imageView3)
+        createBoarder(imageView1)
+        createBoarder(imageView2)
+        createBoarder(imageView3)
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("image1Tapped:"))
+        imageView1.userInteractionEnabled = true
+        imageView2.userInteractionEnabled = true
+        imageView3.userInteractionEnabled = true
+        imageView1.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func createBoarder(view: UIView) {
         let boarderColor : UIColor = UIColor.blackColor()
-        itemDescription.layer.borderColor = boarderColor.CGColor
-        itemDescription.layer.borderWidth = 2.0
-        itemDescription.layer.cornerRadius = 8.0
-        itemDescription.layer.masksToBounds = true
+        view.layer.borderColor = boarderColor.CGColor
+        view.layer.borderWidth = 2.0
+        view.layer.cornerRadius = 8.0
+        view.layer.masksToBounds = true
     }
     
     func textViewDidBeginEditing(textView: UITextView) {
         itemDescription.text = ""
     }
     
-    func imageTapped(sender: UITapGestureRecognizer) {
+    func textViewDidEndEditing(textView: UITextView) {
+        print("end editing")
+        textView.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func image1Tapped(sender: UIImageView) {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self;
+        myPickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(myPickerController, animated: true, completion: nil)
+    }
+    
+    func image2Tapped(sender: UIImageView) {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self;
+        myPickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(myPickerController, animated: true, completion: nil)
+    }
+    
+    func image3Tapped(sender: UIImageView) {
         let myPickerController = UIImagePickerController()
         myPickerController.delegate = self;
         myPickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
@@ -66,7 +113,7 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         
-        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView1.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         
         self.dismissViewControllerAnimated(true, completion: nil)
     }
