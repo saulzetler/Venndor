f6fd73e11c73001c2b88c59ff79eb2e9feda9d2f
saulzetler <saul.zetler49@gmail.com>
2016-06-27 17:21:34 -0400

    Revert "Merge pull request #76 from saulzetler/saul"
    This reverts commit 8602d0f5a68e7894bbdca3e3fddfa2f36a91175f, reversing
    changes made to ee86f5328f323c236aa9bc427ded9ae198cb19ce.

--- venndor/venndor/AppDelegate.swift
+++ venndor/venndor/AppDelegate.swift
@@ -16,10 +16,8 @@ var currentUser: String!
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let googleMapsApiKey = "AIzaSyBGJFI_sQFJZUpVu4cHd7bD5zlV5lra-FU"
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        GMSServices.provideAPIKey(googleMapsApiKey)
         // Override point for customization after application launch.
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
