413d42fc0b1940b0e5d47cbefb12288a56f6c51a
saulzetler <saul.zetler49@gmail.com>
2016-07-06 14:38:20 -0400

    match screen

--- venndor/venndor/OfferViewController.swift
+++ venndor/venndor/OfferViewController.swift
@@ -13,13 +13,16 @@ class OfferViewController: UIViewController, WheelSliderDelegate {
     //screen size for future reference
     let screenSize: CGRect = UIScreen.mainScreen().bounds
     var backgroundImage: UIImage!
+    var item: Item!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBackButton()
         setupWheelSlider()
         setupBackground()
-        setupPrompt()
+        let promptFrame = CGRect(x: 0, y: screenSize.height*0.2, width: screenSize.width, height: screenSize.height*0.2)
+        setupPrompt("How much would you pay?", item: item, screenSize: screenSize, frame: promptFrame)
+//        setupPrompt()
     }
     //to setup the page background to be the items image TO BE DONE
     func setupBackground() {
@@ -59,6 +62,8 @@ class OfferViewController: UIViewController, WheelSliderDelegate {
     
     func offer(sender: UIButton) {
         print("offered")
+        let matchControllerView = PopUpViewControllerSwift()
+        matchControllerView.showInView(self.view, price: 35.22, item: item)
     }
     
     //segue to return to browsing
@@ -75,14 +80,14 @@ class OfferViewController: UIViewController, WheelSliderDelegate {
         }
         let offerButton = makeImageButton("", frame: CGRect(x: wheelslider.frame.width*0.1, y: wheelslider.frame.height*0.1, width: wheelslider.frame.width*0.73, height: wheelslider.frame.height*0.73), target: "offer:", tinted: false, circle: true, backgroundColor: 0x1abc9c, backgroundAlpha: 0)
         wheelslider.addSubview(offerButton)
-        let goImageView = UIImageView(frame: CGRect(x: wheelslider.frame.width*0.35, y: wheelslider.frame.height*0.5, width: wheelslider.frame.width*0.3, height: wheelslider.frame.height*0.3))
+        let goImageView = UIImageView(frame: CGRect(x: wheelslider.frame.width*0.35, y: wheelslider.frame.height*0.55, width: wheelslider.frame.width*0.3, height: wheelslider.frame.height*0.3))
         goImageView.image = UIImage(named: "go.png")
         wheelslider.addSubview(goImageView)
         self.view.addSubview(wheelslider)
     }
     
-    
-    //function to update hte midle number shown in the wheel TO BE DONE
+    //function to update the midle number shown in the wheel TO BE DONE
     func updateSliderValue(value: Double, sender: WheelSlider) {
+        
     }
 }
