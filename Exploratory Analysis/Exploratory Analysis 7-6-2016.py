a1aec24d25f6fd17c028951abe27fa49cfb1de28
austinburks <austinburks.ab@gmail.com>
2016-07-08 22:02:19 -0400

    Market Research Summary
    Added Figures and Summarized Market Trends

--- Exploratory Analysis/Exploratory Analysis 7-6-2016.py
+++ Exploratory Analysis/Exploratory Analysis 7-6-2016.py
@@ -10,6 +10,7 @@ import numpy as np
 from pylab import *
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scipy as sp
 
 #%% Extracting data from csv file
 
@@ -35,19 +36,54 @@ df_market_melt = pd.melt(df_market)
 df_data = pd.merge(df_market_melt,sellers_price,left_on='variable',right_on='Product',how='left').drop('variable',axis=1)
 df_data.rename(columns={'value':'Offer'},inplace=True)
 df_data['OfferMade'] = df_data['Offer'] >= df_data['SellingPrice']
-df_data['Difference'] = df_data['Offer'] - df_data['SellingPrice']
+df_data['Profit'] = (df_data['Offer'] - df_data['SellingPrice'])*0.1
 
+# Discritizing the product prices into 3 categories
+bins = np.linspace(df_data['SellingPrice'].min(),df_data['SellingPrice'].max(),4)
+df_data['PricePoints'] = pd.cut(df_data['SellingPrice'],bins,labels=['Low','Med','High'],include_lowest=True).astype('str')
+df_data['PriceRatio'] = df_data['Profit'].div(df_data['SellingPrice'])
 
-df_grouped = df_data[['SellingPrice','Difference']].astype('float').groupby('SellingPrice',as_index=False).aggregate(np.mean)
+df_grouped = df_data[['SellingPrice','Profit']].astype('float').groupby('SellingPrice',as_index=False).aggregate(np.mean)
 #%%
 ax = sns.factorplot(data = df_data,y='Offer',x='SellingPrice',hue='Product',col='OfferMade',kind='bar')
 
 ax = sns.FacetGrid(data=df_data,hue='OfferMade')
 ax = (ax.map(plt.scatter,'SellingPrice','Offer',s=10*df_data['SellingPrice'].astype(int))).add_legend()
 
-ax = sns.stripplot(data=df_data,x='SellingPrice',y='Difference',hue='OfferMade',split=True)
+ax = sns.stripplot(data=df_data,x='SellingPrice',y='Profit',hue='OfferMade',split=True)
 
-ax = sns.lmplot(data=df_grouped,x='SellingPrice',y='Difference')
+# Linear Regression Plot
+ax = sns.lmplot(data=df_grouped,x='SellingPrice',y='Profit')
+linReg=np.array(sp.stats.linregress(x=df_grouped['SellingPrice'] ,y=df_grouped['Profit']))
+regressionSummary = pd.DataFrame(,columns=[['Slope','Int','RValue','Pvalue','Stderr']])
 
-ax = sns.FacetGrid(data=df_data,col='Product',col_wrap=5)
-ax = ax.map(plt.hist,'Offer')
+ax = sns.FacetGrid(data=df_data,hue='Product')
+ax = ax.map(plt.hist,'Profit',alpha=0.5).add_legend()
+
+ax = sns.boxplot(data=df_data,x='PricePoints',y='Profit',order=['Low','Med','High'],linewidth=4)
+
+# Distributions of Offers
+ax = sns.FacetGrid(data=df_data,hue='PricePoints')
+ax = ax.map(sns.kdeplot
+            ,'Offer'
+            #,bins=np.arange(len( set( df_data['PriceRatio'] ) )+1)-0.5
+            ,shade=True
+            ,linewidth=5).add_legend()
+plt.xlabel('Offer Value',fontsize=20)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.title('KDE Distribution of Offer Value',fontsize=20)
+
+
+# KDE Distributions of Price Ratio
+ax = sns.FacetGrid(data=df_data,hue='PricePoints')
+ax = ax.map(sns.kdeplot
+            ,'PriceRatio'
+            #,bins=np.arange(len( set( df_data['PriceRatio'] ) )+1)-0.5
+            ,shade=True
+            ,linewidth=5).add_legend()
+plt.xlabel('Price Ratio',fontsize=20)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.title('KDE Distribution of Price Ratios',fontsize=20)
+
