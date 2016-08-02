a0bebf555703c602c98284a083e686d021b11007
Tynan Davis <Tynandavis@gmail.com>
2016-08-02 15:32:16 -0400

    did a little stuff

---
+++ venndor/venndor/BoughtController.swift
@@ -0,0 +1,23 @@
+//
+//  BoughtController.swift
+//  venndor
+//
+//  Created by Tynan Davis on 2016-08-02.
+//  Copyright © 2016 Venndor. All rights reserved.
+//
+
+import Foundation
+
+struct BoughtController {
+    func updateSeller(item: Item) {
+        //function should update the sellers item, move the item from posted to sold
+        
+    }
+    func updateBuyer(item: Item) {
+        //function should update the buyer, move the item from matched to bought
+        
+    }
+    func updateMarket(item: Item) {
+        //function should update the market for all users and remove the item from the potential pool of items.
+    }
+}
